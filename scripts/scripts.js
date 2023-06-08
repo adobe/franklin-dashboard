8f0de9cb417a7651c66e0fac3e6c9f8e5bf4e727
Marquise Rosier <mrosier@adobe.com>
2023-06-07 20:13:15 -0700

    no partytown

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -237,13 +237,6 @@ export function addFavIcon(href) {
  * @param {Element} doc The container element
  */
 async function loadLazy(doc) {
-  const main = doc.querySelector('main');
-  await loadBlocks(main);
-
-  const { hash } = window.location;
-  const element = hash ? doc.getElementById(hash.substring(1)) : false;
-  if (hash && element) element.scrollIntoView();
-
   function createInlineScriptSrc(src, parent) {
     const script = document.createElement('script');
     script.type = 'text/javascript';
@@ -253,11 +246,12 @@ async function loadLazy(doc) {
   const ECHARTS = 'https://cdn.jsdelivr.net/npm/echarts@5.0/dist/echarts.min.js';
 
   createInlineScriptSrc(ECHARTS, document.head);
+  const main = doc.querySelector('main');
+  await loadBlocks(main);
 
-  window.partytown = {
-    lib: '/scripts/',
-  };
-  import('./partytown.js');
+  const { hash } = window.location;
+  const element = hash ? doc.getElementById(hash.substring(1)) : false;
+  if (hash && element) element.scrollIntoView();
 
   loadHeader(doc.querySelector('header'));
   loadFooter(doc.querySelector('footer'));

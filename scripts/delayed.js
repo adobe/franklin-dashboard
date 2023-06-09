325956076674c772331599ed069aaf9ff105997d
Damian Langsweirdt <langswei@adobe.com>
2023-06-09 14:29:07 -0500

    load echarts lazy instead of delayed for now

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -4,7 +4,8 @@ import { sampleRUM } from './lib-franklin.js';
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
+// TODO load echarts delayed instead of lazy
 // load echarts
-const script = document.createElement('script');
-script.src = 'https://cdn.jsdelivr.net/npm/echarts@5.4.2/dist/echarts.min.js';
-document.head.appendChild(script);
+// const script = document.createElement('script');
+// script.src = 'https://cdn.jsdelivr.net/npm/echarts@5.4.2/dist/echarts.min.js';
+// document.head.appendChild(script);

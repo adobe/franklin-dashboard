c1455c7d7b34497d99bd830e40131695fc8e732e
Damian Langsweirdt <langswei@adobe.com>
2023-06-09 16:08:45 -0500

    load echarts both delayed and lazy

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -6,6 +6,6 @@ sampleRUM('cwv');
 
 // TODO load echarts delayed instead of lazy
 // load echarts
-// const script = document.createElement('script');
-// script.src = 'https://cdn.jsdelivr.net/npm/echarts@5.4.2/dist/echarts.min.js';
-// document.head.appendChild(script);
+const script = document.createElement('script');
+script.src = 'https://cdn.jsdelivr.net/npm/echarts@5.4.2/dist/echarts.min.js';
+document.head.appendChild(script);

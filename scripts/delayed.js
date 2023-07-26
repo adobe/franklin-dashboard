722c971c02585dccd3e56c4940b831131d839d98
langswei <33439846+langswei@users.noreply.github.com>
2023-07-26 14:22:02 -0500

    load echarts conditionally (#30)

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -4,7 +4,14 @@ import { sampleRUM } from './lib-franklin.js';
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
-const echartScript = document.createElement('script');
-echartScript.type = 'text/javascript';
-echartScript.src = 'https://cdn.jsdelivr.net/npm/echarts@5.4.2/dist/echarts.min.js';
-document.head.appendChild(echartScript);
+// load echarts library unless the page metadata says otherwise
+let loadecharts = true;
+if (document.querySelector('meta[name="echarts"]') && document.querySelector('meta[name="echarts"]').content === 'false') {
+  loadecharts = false;
+}
+if (loadecharts) {
+  const echartScript = document.createElement('script');
+  echartScript.type = 'text/javascript';
+  echartScript.src = 'https://cdn.jsdelivr.net/npm/echarts@5.4.2/dist/echarts.min.js';
+  document.head.appendChild(echartScript);
+}

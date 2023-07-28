8807166a2e25c849011bfef4f1931f8d7a9f0846
Damian Langsweirdt <langswei@adobe.com>
2023-07-28 16:12:27 -0500

    improved metadata read

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,15 +1,11 @@
 /* eslint-disable import/no-cycle */
-import { sampleRUM } from './lib-franklin.js';
+import { sampleRUM, getMetadata } from './lib-franklin.js';
 
 // Core Web Vitals RUM collection
 sampleRUM('cwv');
 
 // load echarts library unless the page metadata says otherwise
-let loadecharts = true;
-if (document.querySelector('meta[name="echarts"]') && document.querySelector('meta[name="echarts"]').content === 'false') {
-  loadecharts = false;
-}
-if (loadecharts) {
+if (getMetadata('echarts') !== 'false') {
   const echartScript = document.createElement('script');
   echartScript.type = 'text/javascript';
   echartScript.src = 'https://cdn.jsdelivr.net/npm/echarts@5.4.2/dist/echarts.min.js';

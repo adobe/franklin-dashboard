0b49cedf0886f79b4e022dfde989ccf26a33a4fa
Marquise Rosier <mrosier@adobe.com>
2023-08-01 11:34:14 -0700

    lint

--- blocks/report/report.js
+++ blocks/report/report.js
@@ -2,7 +2,6 @@ import { readBlockConfig } from '../../scripts/lib-franklin.js';
 import { getQueryInfo, queryRequest, getUrlBase } from '../../scripts/scripts.js';
 
 export default function decorate(block) {
-
   let cfg = readBlockConfig(block);
   cfg = Object.fromEntries(Object.entries(cfg).map(([k, v]) => [k, typeof v === 'string' ? v.toLowerCase() : v]));
   const endpoint = cfg.data;
@@ -23,7 +22,7 @@ export default function decorate(block) {
       window.setTimeout(getQuery, 1);
     } else if (Object.hasOwn(window, 'gettingQueryInfo') && window.gettingQueryInfo === false) {
       queryRequest(cfg, getUrlBase(endpoint));
-      let loaderSpan = document.createElement('div');
+      const loaderSpan = document.createElement('div');
       loaderSpan.className = 'loader';
       block.append(loaderSpan);
     }
@@ -34,23 +33,23 @@ export default function decorate(block) {
       window.setTimeout(makeList, 5);
     } else if (Object.hasOwn(window, flag) && window[flag] === false) {
       // query complete, hide loading graphic
-      let data = window.dashboard[endpoint].results.data;
+      const { data } = window.dashboard[endpoint].results;
       document.querySelectorAll('div.loading').forEach((loading) => {
         loading.style.display = 'none';
       });
-      let main = document.querySelector('main');
-      let loader = main.querySelector('.loader');
+      const main = document.querySelector('main');
+      const loader = main.querySelector('.loader');
       loader.remove();
 
-      let listGridContainer = document.createElement('div');
-      listGridContainer.classList.add('grid', 'list', 'container')
+      const listGridContainer = document.createElement('div');
+      listGridContainer.classList.add('grid', 'list', 'container');
 
-      let cols = ['topurl', 'source', 'actions', 'views', 'actions_per_view', 'pages'];
+      const cols = ['topurl', 'source', 'actions', 'views', 'actions_per_view', 'pages'];
 
-      let listGridHeadingRow = document.createElement('div');
+      const listGridHeadingRow = document.createElement('div');
       listGridHeadingRow.classList.add('grid', 'list', 'row', 'heading');
-      for(let j = 0; j < 6; j++){
-        let listGridHeadings = document.createElement('div');
+      for (let j = 0; j < 6; j += 1) {
+        const listGridHeadings = document.createElement('div');
         listGridHeadings.textContent = cols[j];
         listGridHeadings.classList.add('grid', 'list', 'col', 'heading');
         listGridHeadingRow.appendChild(listGridHeadings);
@@ -58,28 +57,33 @@ export default function decorate(block) {
       listGridContainer.appendChild(listGridHeadingRow);
 
       const map404 = {};
-      for(let i = 0; i < data.length; i++){
-        let { topurl, source, actions, views, actions_per_view, pages } = data[i];
-        source = source ? source : 'empty';
-        if(Object.hasOwn(map404, topurl)){
-            let sourceInfo = {source, actions, views, actions_per_view, pages};
-            map404[topurl].push(sourceInfo);
-        }
-        else{
-            let arr = [];
-            let sourceInfo = {source, actions, views, actions_per_view, pages};
-            arr.push(sourceInfo);
-            map404[topurl] = arr;
+      for (let i = 0; i < data.length; i += 1) {
+        const {
+          topurl, source, actions, views, pages,
+        } = data[i];
+        const actionsPerView = data[i].actions_per_view;
+        const sourceNew = source || 'empty';
+        if (Object.hasOwn(map404, topurl)) {
+          const sourceInfo = {
+            sourceNew, actions, views, actionsPerView, pages,
+          };
+          map404[topurl].push(sourceInfo);
+        } else {
+          const arr = [];
+          const sourceInfo = {
+            sourceNew, actions, views, actionsPerView, pages,
+          };
+          arr.push(sourceInfo);
+          map404[topurl] = arr;
         }
       }
 
-
-      let entries = Object.entries(map404);
+      const entries = Object.entries(map404);
       entries.forEach(([key, val]) => {
-        let listGridRow = document.createElement('div');
+        const listGridRow = document.createElement('div');
         listGridRow.classList.add('grid', 'list', 'row');
 
-        let listGridColumn = document.createElement('div');
+        const listGridColumn = document.createElement('div');
         listGridColumn.classList.add('grid', 'list', 'col', 'topurl');
         const link = document.createElement('a');
         link.href = key;
@@ -87,35 +91,34 @@ export default function decorate(block) {
         listGridColumn.append(link);
         listGridRow.append(listGridColumn);
 
-        let gridElementArray = [];
-        for(let i = 0; i < 5; i++){
-            gridElementArray.push(document.createElement('div'))
-            gridElementArray[i].classList.add('grid', 'list', 'col', cols[i+1], 'flex');
+        const gridElementArray = [];
+        for (let i = 0; i < 5; i += 1) {
+          gridElementArray.push(document.createElement('div'));
+          gridElementArray[i].classList.add('grid', 'list', 'col', cols[i + 1], 'flex');
         }
 
-        for(let k = 0; k < val.length; k++){
-            let keys = Object.keys(val[k]);
-            for(let l = 0; l < 5; l++){
-                let content = val[k][keys[l]];
-                let innerDiv = document.createElement('div');
-                innerDiv.classList.add('grid', 'list', 'col', keys[l], 'inner');
-                if(keys[l] === 'source' && val[k][keys[l]] !== 'empty'){
-                    const link = document.createElement('a');
-                    link.href = content;
-                    link.textContent = content;
-                    innerDiv.append(link);
-                }
-                else{
-                    innerDiv.textContent = content;
-                }
-                gridElementArray[l].append(innerDiv);
+        for (let k = 0; k < val.length; k += 1) {
+          const keys = Object.keys(val[k]);
+          for (let l = 0; l < 5; l += 1) {
+            const content = val[k][keys[l]];
+            const innerDiv = document.createElement('div');
+            innerDiv.classList.add('grid', 'list', 'col', keys[l], 'inner');
+            if (keys[l] === 'source' && val[k][keys[l]] !== 'empty') {
+              const srcLink = document.createElement('a');
+              srcLink.href = content;
+              srcLink.textContent = content;
+              innerDiv.append(srcLink);
+            } else {
+              innerDiv.textContent = content;
             }
+            gridElementArray[l].append(innerDiv);
+          }
         }
-        for(let i = 0; i < 5; i++){
-            listGridRow.append(gridElementArray[i]);
+        for (let i = 0; i < 5; i += 1) {
+          listGridRow.append(gridElementArray[i]);
         }
         listGridContainer.append(listGridRow);
-        });
+      });
       block.append(listGridContainer);
     }
   };

0db85d7134f411d83ce809d6f140ef43f5e94387
Damian Langsweirdt <langswei@adobe.com>
2023-08-08 11:02:03 -0500

    domainlist block

---
+++ blocks/domainlist/domainlist.js
@@ -0,0 +1,116 @@
+import { readBlockConfig } from '../../scripts/lib-franklin.js';
+import { getQueryInfo, queryRequest, getUrlBase } from '../../scripts/scripts.js';
+import { drawLoader, hideLoader } from '../../scripts/loader.js';
+
+export default function decorate(block) {
+  let cfg = readBlockConfig(block);
+  cfg = Object.fromEntries(Object.entries(cfg).map(([k, v]) => [k, typeof v === 'string' ? v.toLowerCase() : v]));
+  const endpoint = cfg.data;
+
+  cfg.block = block;
+  const flag = `${endpoint}Flag`;
+
+  // once we read config, clear the dom.
+  block.querySelectorAll(':scope > div').forEach((row) => {
+    row.style.display = 'none';
+  });
+
+  const getQuery = () => {
+    if (!Object.hasOwn(window, 'gettingQueryInfo')) {
+      getQueryInfo();
+    }
+    if (Object.hasOwn(window, 'gettingQueryInfo') && window.gettingQueryInfo === true) {
+      window.setTimeout(getQuery, 1);
+    } else if (Object.hasOwn(window, 'gettingQueryInfo') && window.gettingQueryInfo === false) {
+      setTimeout(() => {
+        queryRequest(cfg, getUrlBase(endpoint));
+      }, 3000);
+
+      drawLoader(block);
+    }
+  };
+
+  const makeList = () => {
+    if ((Object.hasOwn(window, flag) && window[flag] === true) || !Object.hasOwn(window, flag)) {
+      window.setTimeout(makeList, 5);
+    } else if (Object.hasOwn(window, flag) && window[flag] === false) {
+      // query complete, hide loading graphic
+      const { data } = window.dashboard[endpoint].results;
+      hideLoader(block);
+
+      // using a table instead of divs to leverage jquery tablesorter
+      const table = document.createElement('table');
+      table.classList.add('tablesorter');
+
+      // prepare header row
+      const thead = document.createElement('thead');
+      const header = document.createElement('tr');
+      const header1 = document.createElement('th');
+      header1.textContent = 'Domain';
+      const header2 = document.createElement('th');
+      header2.classList.add('right');
+      header2.textContent = 'First Visit';
+      const header3 = document.createElement('th');
+      header3.classList.add('right');
+      header3.textContent = 'Last Visit';
+      const header4 = document.createElement('th');
+      header4.classList.add('right');
+      header4.textContent = 'Current Month Est. Visits';
+      const header5 = document.createElement('th');
+      header5.classList.add('right');
+      header5.textContent = 'Total Est. Visits';
+      header.appendChild(header1);
+      header.appendChild(header2);
+      header.appendChild(header3);
+      header.appendChild(header4);
+      header.appendChild(header5);
+      thead.appendChild(header);
+      table.appendChild(thead);
+
+      // prepare body rows
+      const tbody = document.createElement('tbody');
+      for (let i = 0; i < data.length; i += 1) {
+        const row = document.createElement('tr');
+        const col1 = document.createElement('td');
+        col1.textContent = data[i].hostname;
+        const col2 = document.createElement('td');
+        col2.classList.add('right');
+        col2.textContent = data[i].first_visit;
+        const col3 = document.createElement('td');
+        col3.classList.add('right');
+        col3.textContent = data[i].last_visit;
+        const col4 = document.createElement('td');
+        col4.classList.add('right');
+        col4.textContent = parseInt(data[i].current_month_visits || 0, 10).toLocaleString('en-US');
+        const col5 = document.createElement('td');
+        col5.classList.add('right');
+        col5.textContent = parseInt(data[i].total_visits, 10).toLocaleString('en-US');
+        row.appendChild(col1);
+        row.appendChild(col2);
+        row.appendChild(col3);
+        row.appendChild(col4);
+        row.appendChild(col5);
+        tbody.appendChild(row);
+      }
+      table.appendChild(tbody);
+
+      // add table to block and add sort functionality
+      block.appendChild(table);
+      // eslint-disable-next-line no-undef
+      $('table.tablesorter').tablesorter();
+
+      if (data.length === 0) {
+        const noresults = document.createElement('p');
+        const params = new URLSearchParams(window.location.search);
+        if (params.has('domainkey')) {
+          noresults.textContent = 'No results found.';
+        } else {
+          noresults.innerHTML = '<i>domainkey</i> is required.  Please provide <a href="/all-domains/">here</a>.';
+        }
+        block.append(noresults);
+      }
+    }
+  };
+  getQuery();
+  makeList();
+}
--- scripts/delayed.js
+++ scripts/delayed.js
@@ -11,3 +11,21 @@ if (getMetadata('echarts') !== 'false') {
   echartScript.src = 'https://cdn.jsdelivr.net/npm/echarts@5.4.2/dist/echarts.min.js';
   document.head.appendChild(echartScript);
 }
+
+// load jquery for tablesorter if the page metadata declares
+if (getMetadata('jquery') === 'true') {
+  const jqueryScript = document.createElement('script');
+  jqueryScript.type = 'text/javascript';
+  jqueryScript.src = 'https://code.jquery.com/jquery-3.7.0.min.js';
+  document.head.appendChild(jqueryScript);
+
+  const sortScript = document.createElement('script');
+  sortScript.type = 'text/javascript';
+  sortScript.src = 'https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.3/js/jquery.tablesorter.min.js';
+  document.head.appendChild(sortScript);
+
+  const sortStyle = document.createElement('link')
+  sortStyle.rel = 'stylesheet';
+  sortStyle.href = 'https://cdnjs.cloudflare.com/ajax/libs/jquery.tablesorter/2.31.3/css/theme.default.min.css';
+  document.head.appendChild(sortStyle);
+}

e620dbb3f301424d9397fb8fe778000869bfccbb
Marquise Rosier <mrosier@Marquises-MacBook-Pro.local>
2023-11-13 23:41:15 -0800

    spec

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -12,24 +12,24 @@ const loadScript = (src, type) => {
   return thisScript;
 }
 
-loadCSS("/node_modules/@spectrum-css/vars/dist/spectrum-global.css")
-loadCSS("/node_modules/@spectrum-css/vars/dist/spectrum-global.css")
-loadCSS("/node_modules/@spectrum-css/vars/dist/spectrum-medium.css")
-loadCSS("/node_modules/@spectrum-css/vars/dist/spectrum-light.css")
-loadCSS("/node_modules/@spectrum-css/page/dist/index-vars.css")
-loadCSS("/node_modules/@spectrum-css/button/dist/index-vars.css")
-loadCSS("/node_modules/@spectrum-css/table/dist/index-vars.css")
-loadCSS("/node_modules/@spectrum-css/progressbar/dist/index-vars.css");
-loadCSS("/node_modules/@spectrum-css/badge/dist/index-vars.css")
-loadCSS("/node_modules/@spectrum-css/accordion/dist/index-vars.css")
+loadCSS("/styles/node_modules/@spectrum-css/vars/dist/spectrum-global.css")
+loadCSS("/styles/node_modules/@spectrum-css/vars/dist/spectrum-global.css")
+loadCSS("/styles/node_modules/@spectrum-css/vars/dist/spectrum-medium.css")
+loadCSS("/styles/node_modules/@spectrum-css/vars/dist/spectrum-light.css")
+loadCSS("/styles/node_modules/@spectrum-css/page/dist/index-vars.css")
+loadCSS("/styles/node_modules/@spectrum-css/button/dist/index-vars.css")
+loadCSS("/styles/node_modules/@spectrum-css/table/dist/index-vars.css")
+loadCSS("/styles/node_modules/@spectrum-css/progressbar/dist/index-vars.css");
+loadCSS("/styles/node_modules/@spectrum-css/badge/dist/index-vars.css")
+loadCSS("/styles/node_modules/@spectrum-css/accordion/dist/index-vars.css")
 
 // load echarts library unless the page metadata says otherwise
 if (getMetadata('echarts') !== 'false') {
   loadScript('https://cdn.jsdelivr.net/npm/echarts@5.4.2/dist/echarts.min.js', 'text/javascript');
 }
 
-loadScript('/node_modules/loadicons/index.js', 'text/javascript');
-loadScript('', 'text/javascript').textContent = `  loadIcons('/node_modules/@spectrum-css/icon/dist/spectrum-css-icons.svg');`
+loadScript('/styles/node_modules/loadicons/index.js', 'text/javascript');
+loadScript('', 'text/javascript').textContent = `  loadIcons('/styles/node_modules/@spectrum-css/icon/dist/spectrum-css-icons.svg');`
 
 const html = document.querySelector('html');
 html.className = "spectrum spectrum--medium spectrum--light"

e0ba8ac83b778dddb9070b38590ce9dd2e828969
David Barrat <david@barrat.io>
2023-11-30 21:53:44 +0100

    component: quickchart via helix component

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,6 +7,7 @@ const config = {
     "@storybook/preset-create-react-app",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
+    "@storybook/addon-knobs",
   ],
   framework: {
     name: "@storybook/react-webpack5",
---
+++ src/components/QuickChart/QuickChart.stories.js
@@ -0,0 +1,26 @@
+import QuickChart from './QuickChart.jsx';
+
+export default {
+  title: 'Design System/QuickChart',
+  component: QuickChart,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'fullscreen',
+  },
+};
+
+const emptyDomainKey = '';
+
+export const aemdotlive = {
+  args: {
+    url: 'www.aem.live',
+    domainKey: emptyDomainKey,
+  },
+};
+
+export const adobedotcom = {
+  args: {
+    url: 'www.adobe.com',
+    domainKey: emptyDomainKey,
+  },
+};

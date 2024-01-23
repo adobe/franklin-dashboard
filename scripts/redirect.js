c60dd8d6d3de82e6e61afda41bbc8b05fae0ba70
Marquise Rosier <mrosier@marquises-mbp.corp.adobe.com>
2024-01-23 12:31:37 -0800

    make redirect script robust

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -1,3 +1,19 @@
-if (new RegExp("hlx\\.live$").test(location.hostname)) {
-    location.replace("https://datah.aem.live")
+if (new RegExp("hlx\\.live$").test(location.hostname) || new RegExp("hlx\\.page$").test(location.hostname)) {
+    const queryParams = new URLSearchParams(location.search);
+    const newQp = new URLSearchParams();
+    const qpList = ['domainkey', 'startdate', 'enddate', 'url'];
+    let requiredQueryParamsFlag = true;
+    qpList.forEach((qp) => {
+        if(!queryParams.has(qp)){
+            requiredQueryParamsFlag = false;
+        }else{
+            newQp.set(qp, queryParams.get(qp));
+        }
+    });
+
+    if(requiredQueryParamsFlag){
+        location.href("https://data.aem.live" + newQp.toString());
+    }else{
+        location.href("https://data.aem.live");
+    }
 }

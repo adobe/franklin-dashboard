7b10c995520aaf01d48b7cd313769d9184bb8356
Marquise Rosier <mrosier@marquises-mbp.corp.adobe.com>
2024-01-23 14:45:25 -0800

    linting

--- scripts/redirect.js
+++ scripts/redirect.js
@@ -1,19 +1,20 @@
-if (new RegExp("hlx\\.live$").test(location.hostname) || new RegExp("hlx\\.page$").test(location.hostname)) {
-    const queryParams = new URLSearchParams(location.search);
-    const newQp = new URLSearchParams();
-    const qpList = ['domainkey', 'startdate', 'enddate', 'url'];
-    let requiredQueryParamsFlag = true;
-    qpList.forEach((qp) => {
-        if(!queryParams.has(qp)){
-            requiredQueryParamsFlag = false;
-        }else{
-            newQp.set(qp, queryParams.get(qp));
-        }
-    });
-
-    if(requiredQueryParamsFlag){
-        location.href = "https://data.aem.live/rum-dashboard?" + newQp.toString();
-    }else{
-        location.href = "https://data.aem.live";
+/* eslint-disable no-restricted-globals */
+if (/hlx\\.live$/.test(location.hostname) || /hlx\\.page$/.test(location.hostname)) {
+  const queryParams = new URLSearchParams(location.search);
+  const newQp = new URLSearchParams();
+  const qpList = ['domainkey', 'startdate', 'enddate', 'url'];
+  let requiredQueryParamsFlag = true;
+  qpList.forEach((qp) => {
+    if (!queryParams.has(qp)) {
+      requiredQueryParamsFlag = false;
+    } else {
+      newQp.set(qp, queryParams.get(qp));
     }
+  });
+
+  if (requiredQueryParamsFlag) {
+    location.href = `https://data.aem.live/rum-dashboard?${newQp.toString()}`;
+  } else {
+    location.href = 'https://data.aem.live';
+  }
 }

cb9308153d4021acb47744606a3291441b7cb809
Marquise Rosier <mrosier@adobe.com>
2024-01-24 09:03:19 -0800

    Redirection (#63)

---
+++ scripts/redirect.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-restricted-globals */
+/* eslint-disable prefer-regex-literals */
+if (new RegExp('hlx\\.live$').test(location.hostname) || new RegExp('hlx\\.page$').test(location.hostname)) {
+  const queryParams = new URLSearchParams(location.search);
+  const newQp = new URLSearchParams();
+  const qpList = ['domainkey', 'startdate', 'enddate', 'url'];
+  let requiredQueryParamsFlag = true;
+  qpList.forEach((qp) => {
+    if (!queryParams.has(qp) || queryParams.get(qp) === null || queryParams.get(qp) === '') {
+      requiredQueryParamsFlag = false;
+    } else {
+      newQp.set(qp, queryParams.get(qp));
+    }
+  });
+
+  if (requiredQueryParamsFlag) {
+    location.href = `https://data.aem.live/rum-dashboard?${newQp.toString()}`;
+  } else {
+    location.href = 'https://data.aem.live';
+  }
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,3 @@
-import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';

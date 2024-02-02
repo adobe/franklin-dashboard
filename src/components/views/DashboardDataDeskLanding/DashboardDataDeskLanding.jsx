4d7a2b9d55321a53eac1d63075a4466d748ae596
Marquise Rosier <mrosier@Marquises-MacBook-Pro.local>
2024-02-01 23:22:34 -0800

    fix redirec

--- src/components/views/DashboardDataDeskLanding/DashboardDataDeskLanding.jsx
+++ src/components/views/DashboardDataDeskLanding/DashboardDataDeskLanding.jsx
@@ -40,7 +40,7 @@ const DashboardDataDeskLanding = () => {
                       setGlobalUrl(formGlobalKey);
                       setDomainKey(formDomainKey);
                       const dates = intervalOffsetToDates(0, 30);
-                      location.href = `rum-dashboard?url=${formGlobalKey}&domainkey=${formDomainKey}&startdate=${dates['start']}&enddate=${dates['end']}`;
+                      location.href = `https://data.aem.live/rum-dashboard?url=${formGlobalKey}&domainkey=${formDomainKey}&startdate=${dates['start']}&enddate=${dates['end']}`;
                     }}
                 />
                 <span>{domainkey}</span>

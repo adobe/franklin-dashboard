2f7e409366a870113128ccc745e3af575cfd2353
Damian Langsweirdt <langswei@adobe.com>
2024-02-16 15:21:45 -0600

    fix: keep all data for chart

--- src/components/charts/LineChart/LineChart.jsx
+++ src/components/charts/LineChart/LineChart.jsx
@@ -1,14 +1,14 @@
 import {
   ResponsiveContainer, LineChart, CartesianGrid, XAxis, YAxis, Tooltip, Legend, Line,
 } from 'recharts';
-import './LineChart.css'
+import './LineChart.css';
 
-function getMinMax(data, y_datakey){
+function getMinMax(data, y_datakey) {
   let max = 0;
   data.forEach((item) => {
     const currData = item[y_datakey];
     max = Math.max(max, parseInt(currData, 10));
-  })
+  });
   return [0, max];
 }
 
@@ -18,23 +18,24 @@ export function DashboardLineChart({
   width = '100%',
   x_datakey,
   y_datakey,
-}){ 
+}) {
   return (
     <div style={{
       height,
       width,
     }}>
- <ResponsiveContainer  height="100%" width="100%">
+ <ResponsiveContainer height="100%" width="100%">
       <LineChart
           width={width}
           height={height}
-          data={data.slice(1)}
+          data={data}
         >
-          <XAxis type={'category'} dataKey={x_datakey} tickFormatter={(value, index) => {return new Date(value).toLocaleDateString('en-US')}} tick={{fontSize: '12px'}} angle={-45} reversed={true}/>
-          <YAxis domain={getMinMax(data, y_datakey)} tickFormatter={(value, index) => { return parseInt(value, 10).toLocaleString('en-US')}} scale='auto' fontSize={'12px'}/>
-          <Tooltip formatter={(value, index) => {return parseInt(value, 10).toLocaleString('en-US')}} />
+          <XAxis type={'category'} dataKey={x_datakey} tickFormatter={(value, index) => new Date(value).toLocaleDateString('en-US')} tick={{ fontSize: '12px' }} angle={-45} reversed={true}/>
+          <YAxis domain={getMinMax(data, y_datakey)} tickFormatter={(value, index) => parseInt(value, 10).toLocaleString('en-US')} scale='auto' fontSize={'12px'}/>
+          <Tooltip formatter={(value, index) => parseInt(value, 10).toLocaleString('en-US')} />
           <Line type="monotone" dataKey={y_datakey} strokeWidth={3} stroke="#8884d8" dot={false} />
         </LineChart>
       </ResponsiveContainer>
       </div>
-)};
+  );
+}

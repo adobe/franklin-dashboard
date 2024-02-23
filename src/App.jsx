2c6074550538eb45b58178ab214f13f875e879ab
Subodh Jha <subodhj@subodhs-laptop.corp.adobe.com>
2024-02-23 10:36:57 +0530

    FORMS-13240: Creating Forms RUM Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import { ToastContainer } from '@react-spectrum/toast';
 
 import DashboardDataDeskLanding from './components/views/DashboardDataDeskLanding/DashboardDataDeskLanding.jsx';
 import Dashboard404Report from './components/views/Dashboard404Report/Dashboard404Report.jsx';
-import DashboardRumView from './components/views/DashboardRUMView/DashboardRUMView.jsx';
+import DashboardRumView from './components/views/FormsDashboardRUMView/DashboardRUMView.jsx';
 import DashboardRUMPerformanceMonitor from './components/views/DashboardRUMPerformanceMonitor/DashboardRUMPerformanceMonitor.jsx';
 
 import './App.css';

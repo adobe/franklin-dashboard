b9a6b8581784797ac2afe9b3f63f8d13e41f208e
Subodh Jha <subodhj@Subodhs-Laptop.local>
2024-02-29 19:21:16 +0530

    Optimizing performance

--- src/components/views/FormsDashboardRUMView/RumTableView.jsx
+++ src/components/views/FormsDashboardRUMView/RumTableView.jsx
@@ -32,6 +32,7 @@ export function RumTableView({
     }
 
   useEffect( async () => {
+    f(window.dashboard["rum-dashboard"]){
   await queryRequest("rum-checkpoint-urls", "https://helix-pages.anywhere.run/helix-services/run-query@v3/", {}, 'submit', `${data[0]['url']}`);
   await queryRequest("rum-dashboard", "https://helix-pages.anywhere.run/helix-services/run-query@v3/", {}, 'cwv', `${data[0]['url']}`);
   console.log(window.dashboard["rum-dashboard"]);
@@ -43,6 +44,7 @@ cwvData.forEach(data => {
     urlMap[data.url] = data;
 });
   setFlag(true);
+}
 }, [flag]);
     return (
       data.length > 0  && flag && <TableView width="100%" height="100%" alignSelf="end" overflowMode='truncate' selectionMode='multiple' selectionStyle='highlight' density='compact' id='tableview'>

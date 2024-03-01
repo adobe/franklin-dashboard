f065d96c96ebd6aa28d8dd4defb7b767b4cf883d
Subodh Jha <subodhj@subodhs-laptop.corp.adobe.com>
2024-03-01 17:06:11 +0530

    Refactoring code

--- src/components/views/FormsDashboardRUMView/RumDashboardMain.jsx
+++ src/components/views/FormsDashboardRUMView/RumDashboardMain.jsx
@@ -41,7 +41,7 @@ export function RumDashboardMain() {
               <DashboardQueryFilter hasCheckpointField={false} hasUrlField={true} hasDomainkeyField={true} isReport={false}
               data={data} setter={setData} dataEndpoint={'rum-checkpoint-urls'}
               apiEndpoint={'https://helix-pages.anywhere.run/helix-services/run-query@v3'}
-              dataFlag={fetchFlag} flagSetter={setFetchFlag} config={config} configSetter={setConfig}>
+              dataFlag={fetchFlag} flagSetter={setFetchFlag} config={config} configSetter={setConfig} formsURL={true}>
               </DashboardQueryFilter>
             </View>
 

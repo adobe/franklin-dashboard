9f25f6575a62bfbc7211751a428f20dde057d6f5
Subodh Jha <subodhj@subodhs-laptop.corp.adobe.com>
2024-03-01 16:59:15 +0530

    Refactoring code

--- src/components/views/FormsDashboardRUMView/DashboardRUMView.jsx
+++ src/components/views/FormsDashboardRUMView/DashboardRUMView.jsx
@@ -1,5 +1,5 @@
 import DashboardLayout from '../../core/Layout/Layout.jsx';
-import { RumDashboardMain } from './RumDashboardMain.jsx';
+import { RumDashboardMain } from '../DashboardRUMView/RumDashboardMain.jsx';
 import './RumDashboardMain.css';
 
 const DashboardRUMView = () => (

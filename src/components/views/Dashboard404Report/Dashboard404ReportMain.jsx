45ce5d54225c3b005c1d3d04d47abf2c9936c71b
Damian Langsweirdt <langswei@adobe.com>
2024-04-01 13:33:00 -0500

    fix: use new 404 query, lint

--- src/components/views/Dashboard404Report/Dashboard404ReportMain.jsx
+++ src/components/views/Dashboard404Report/Dashboard404ReportMain.jsx
@@ -1,8 +1,13 @@
-import { Grid, View, Flex, Well, Text } from '@adobe/react-spectrum';
+/* eslint-disable no-unused-vars */
+import {
+  Grid, View, Flex, Well, Text,
+} from '@adobe/react-spectrum';
 import { useState, useEffect } from 'react';
-import DashboardQueryFilter from '../../../controllers/Filters/DashboardQueryFilter';
-import { RumTableView } from './Dashboard404TableView';
+// eslint-disable-next-line import/no-named-as-default
+import DashboardQueryFilter from '../../../controllers/Filters/DashboardQueryFilter.jsx';
+import { RumTableView } from './Dashboard404TableView.jsx';
 
+// eslint-disable-next-line import/prefer-default-export
 export function RumDashboardMain() {
   const [data, setData] = useState([]);
   const [fetchFlag, setFetchFlag] = useState(false);
@@ -11,34 +16,41 @@ export function RumDashboardMain() {
   useEffect(() => {
   }, [data, fetchFlag]);
 
-  const columns = ['topurl', 'source', 'views'];
+  const columns = ['url', 'top_source', 'source_count', 'views'];
   const columnHeadings = {
-    topurl: ['404 URL', 'A URL that is returning 404 Not Found status code indicates that the server is working; but unable to find the file being requested.'],
-    source: ['Source', 'URL or Checkpoint that led visitor to visit unfound page.'],
-    views: ['Pageviews', 'Number of visitors who have been directed to a 404 from the URL or Checkpoint in the Source Column..'],
+    url: ['404 URL', 'A URL that is returning 404 Not Found status code indicates that the server is working; but unable to find the file being requested.'],
+    top_source: ['Top Source', 'The most frequent referer for this URL.'],
+    source_count: ['Source Count', 'The number of detected referers for this URL.'],
+    views: ['Pageviews', 'Number of visitors who have been directed to a 404 from the URL.'],
   };
 
   return (
     <Grid areas={['heading heading',
-    'sidebar content1',
-    'sidebar content1']} columns={['.5fr', '6fr']} rows={['.5fr', '6fr']} height="87vh" width="100%" columnGap={'size-100'} id='table_gridview'>
+      'sidebar content1',
+      'sidebar content1']} columns={['.5fr', '6fr']} rows={['.5fr', '6fr']} height="87vh" width="100%" columnGap={'size-100'} id='table_gridview'>
             <View gridArea={'heading'}>
-              <Well UNSAFE_style={{textAlign: 'center'}}>
-                <Text>To repair any links that show up in the <b>404 URL</b> column below, open up your site's <code>redirects.xslx</code> and add a redirect from the url reported in the <b>404 URL</b> column below.</Text>
+              <Well UNSAFE_style={{ textAlign: 'center' }}>
+                <Text>
+                  To repair any links that show up in the <b>404 URL</b>
+                  column below, open up your site's <code>redirects.xslx</code>
+                  and add a redirect from the url reported in the <b>404 URL</b> column below.
+                </Text>
               </Well>
             </View>
             <View>
-              <DashboardQueryFilter hasCheckpointField={false} hasUrlField={true} hasDomainkeyField={true} isReport={false}
-              data={data} setter={setData} dataEndpoint={'rum-sources'}
-              apiEndpoint={'https://helix-pages.anywhere.run/helix-services/run-query@v3'}
-              dataFlag={fetchFlag} flagSetter={setFetchFlag} config={config} configSetter={setConfig}>
+              <DashboardQueryFilter
+                hasCheckpointField={false} hasUrlField={true} hasDomainkeyField={true}
+                isReport={false} data={data} setter={setData} dataEndpoint={'rum-404'}
+                apiEndpoint={'https://helix-pages.anywhere.run/helix-services/run-query@v3'}
+                dataFlag={fetchFlag} flagSetter={setFetchFlag} config={config}
+                configSetter={setConfig}>
               </DashboardQueryFilter>
             </View>
 
             <View gridArea="content1" width="100%" height="100%" overflow="hidden">
               <Flex width="100%" height="100%">
-                <RumTableView 
-                data={data} setter={setData} dataFlag={fetchFlag} flagSetter={setFetchFlag} width="100%" height="100%" columns={columns} 
+                <RumTableView
+                data={data} setter={setData} dataFlag={fetchFlag} flagSetter={setFetchFlag} width="100%" height="100%" columns={columns}
                 columnHeadings={columnHeadings} config={config} configSetter={setConfig}/>
               </Flex>
             </View>
--- src/components/views/Dashboard404Report/Dashboard404TableView.jsx
+++ src/components/views/Dashboard404Report/Dashboard404TableView.jsx
@@ -1,12 +1,13 @@
+/* eslint-disable no-unused-vars */
 import {
   TableView, TableHeader, TableBody, Row, Cell, Column,
-
   Badge, Text, ProgressBar, ContextualHelp, Content, Heading, IllustratedMessage, Divider, Button,
 } from '@adobe/react-spectrum';
 import './Dashboard404TableView.css';
+// eslint-disable-next-line import/extensions
 import NotFound from '@spectrum-icons/illustrations/NotFound';
-import { useStore } from 'stores/global';
 
+// eslint-disable-next-line import/prefer-default-export
 export function RumTableView({
   data, dataFlag, columns, columnHeadings,
 }) {
@@ -15,65 +16,65 @@ export function RumTableView({
       data.length > 0
               && <TableView width="100%" height="100%" alignSelf="end" overflowMode='truncate' selectionMode='multiple' selectionStyle='highlight' density='compact' id='tableview404'>
                   <TableHeader>
-                      {(
-                          columns.map((key) => {
-                            if (key === 'url') {
-                              const hostname = data[0][key] ? new URL(data[0][key].startsWith('https://') ? data[0][key] : `https://${data[0][key]}`).hostname : '';
-                              return <Column align="start" width="fit-content" allowsResizing={true}>{`${key} (${hostname})`}</Column>;
-                            }
-                            return (
-                                  <Column align="start">
-                                      <ContextualHelp variant="info">
-                                          <Heading>{columnHeadings[key][0]}</Heading>
-                                          <Divider size='M'></Divider>
-                                          <Content>
-                                              <Text>{columnHeadings[key][1]}</Text>
-                                          </Content>
-                                      </ContextualHelp>
-                                      <Text>{columnHeadings[key][0]}</Text>
-                                  </Column>
-                            );
-                          })
-                      )}
+                    {(
+                      columns.map((key) => {
+                        if (key === 'url') {
+                          const hostname = data[0][key] ? new URL(data[0][key].startsWith('https://') ? data[0][key] : `https://${data[0][key]}`).hostname : '';
+                          return <Column align="start" width="fit-content" allowsResizing={true}>{`URL (${hostname})`}</Column>;
+                        }
+                        return (
+                          <Column align="start">
+                              <ContextualHelp variant="info">
+                                  <Heading>{columnHeadings[key][0]}</Heading>
+                                  <Divider size='M'></Divider>
+                                  <Content>
+                                      <Text>{columnHeadings[key][1]}</Text>
+                                  </Content>
+                              </ContextualHelp>
+                              <Text>{columnHeadings[key][0]}</Text>
+                          </Column>
+                        );
+                      })
+                    )}
                   </TableHeader>
                   <TableBody>
                       {
                           data.map((rum, idx) => <Row key={idx} width="100%" height="size-1000" >
-                                  {(
-                                      columns.map((col) => {
-                                        if (col === 'topurl') {
-                                          return <Cell><a href={rum[col]} target="_blank">{rum[col]}</a></Cell>;
-                                        }
-                                        if (col === 'source') {
-                                          if (rum[col].startsWith('https://')) {
-                                            return <Cell><a href={rum[col]} target="_blank">{rum[col]}</a></Cell>;
-                                          }
-                                          return <Cell>{rum[col]}</Cell>;
-                                        }
-                                        return (
-                                        <Cell width="size-1000">
-                                            <Badge width="size-1000" alignSelf='center' variant='info'>
-                                                <Text>{parseInt(rum[col], 10).toLocaleString('en-US')}</Text>
-                                            </Badge>
-                                        </Cell>
-                                        );
-                                      })
-                                  )}
-                              </Row>)
+                              {(
+                                columns.map((col) => {
+                                  if (col === 'url') {
+                                    return <Cell><a href={rum[col]} target="_blank">{rum[col]}</a></Cell>;
+                                  }
+                                  if (col === 'top_source') {
+                                    if (rum[col].startsWith('https://')) {
+                                      return <Cell><a href={rum[col]} target="_blank">{rum[col]}</a></Cell>;
+                                    }
+                                    return <Cell>{rum[col]}</Cell>;
+                                  }
+                                  return (
+                                  <Cell width="size-1000">
+                                      <Badge width="size-1000" alignSelf='center' variant='info'>
+                                          <Text>{parseInt(rum[col], 10).toLocaleString('en-US')}</Text>
+                                      </Badge>
+                                  </Cell>
+                                  );
+                                })
+                              )}
+                            </Row>)
                       }
                   </TableBody>
               </TableView>
     );
   } if (dataFlag) {
     return (
-              <ProgressBar margin="auto" label="Loading…" isIndeterminate />
+      <ProgressBar margin="auto" label="Loading…" isIndeterminate />
     );
   }
   return (
-              <IllustratedMessage margin="auto">
-                  <NotFound />
-                  <Heading>No results</Heading>
-                  <Content>Use Filters</Content>
-              </IllustratedMessage>
+    <IllustratedMessage margin="auto">
+        <NotFound />
+        <Heading>No results</Heading>
+        <Content>Use Filters</Content>
+    </IllustratedMessage>
   );
 }

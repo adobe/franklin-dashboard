a223ab4a7a84835958b0400e63ddc4a4538376ac
Subodh Jha <subodhj@Subodhs-Laptop.local>
2024-04-05 12:02:20 +0530

    Changing dashboardqueryfilter change

--- src/connectors/utils.js
+++ src/connectors/utils.js
@@ -237,7 +237,7 @@ export async function  getBaseDomains(endpoint, endpointHost, qps = {}, flagSett
   let totalFormViews = 0;
   let totalFormSubmissions = 0;
   let viewData = [];
-  const qpsparameter = {'offset': 0, 'limit': 500};
+  const qpsparameter = {'offset': -1, 'limit': 500};
   do {
       try {
           // Make the queryRequest
@@ -248,23 +248,19 @@ export async function  getBaseDomains(endpoint, endpointHost, qps = {}, flagSett
           console.log("---domain------");
           for (let i = 0; i < data.length; i += 1) {
               let domain = data[i]['url'].replace(/^http(s)*:\/\//, '').split('/')[0]
-              if ((domain.indexOf('bamboohr')>-1)) {
+              if (!domain.endsWith('hlx.page') && !domain.endsWith('hlx.live') && !(domain.indexOf('localhost')>-1)
+                  && !(domain.indexOf('dev')>-1) && !(domain.indexOf('stage')>-1) && !(domain.indexOf('stagging')>-1) && !(domain.indexOf('main-')>-1)
+                  && !(domain.indexOf('staging')>-1) && !(domain.indexOf('about:srcdoc')>-1)) {
                   domains.add(domain);
                       totalFormViews = totalFormViews + Number(data[i]['views']);
                       totalFormSubmissions = totalFormSubmissions + Number(data[i]['submissions']);
                       let found = false;
                       for (let j = 0; j < viewData.length; j++) {
+                          console.log(data[i]['url']);
                           if (viewData[j]['url'].includes(domain) && !duplicateDomain.has(data[i]['url'])) {
                               duplicateDomain.add(data[i]['url']);
-                              console.log(data[i]['url']);
                               viewData[j]['views'] += Number(data[i]['views']);
                               viewData[j]['submissions'] += Number(data[i]['submissions']);
-                              console.log(viewData[j]['url']);
-                              console.log(data[i]['url']);
-                              console.log(data[i]['views']);
-                              console.log(data[i]['submissions']);
-                              console.log(viewData[j]['views']);
-                              console.log(viewData[j]['submissions']);
                               found = true;
                               break;
                           }

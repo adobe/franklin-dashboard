7d8657b71b59cb09298eeb362a3858bf087af61b
Subodh Jha <subodhj@subodhs-laptop.corp.adobe.com>
2024-08-12 14:43:40 +0530

    Adding CS RUM Data

--- src/connectors/utils.js
+++ src/connectors/utils.js
@@ -345,7 +345,9 @@ export async function  getEDSCSFormSubmission(endpoint, endpointHost, qps = {}, 
     url: 'ALL',
     submissions: totalFormSubmissions
 });
-  const qpsdomainparameter = {'offset': -1, 'limit': 500 ,'url': domains};
+const domainsArray = Array.from(domains);
+const domainsString = domainsArray.join(', ');
+  const qpsdomainparameter = {'offset': -1, 'limit': 500 ,'url': domainsString};
   await queryRequest('dash/domain-list', endpointHost, qpsdomainparameter);
   window.dashboard[endpoint].results.data = viewData;
   window.dashboard['internalCSRUMDataLoaded'] = true;

ff72b0f2103ec45402a151d791c33f7a4c4067fe
Subodh Jha <subodhj@Subodhs-Laptop.local>
2024-08-13 14:37:03 +0530

    Adding CS RUM Data

--- src/controllers/Filters/DashboardQueryFilter.jsx
+++ src/controllers/Filters/DashboardQueryFilter.jsx
@@ -100,8 +100,11 @@ export function DashboardQueryFilter({
         setter([]);
         flagSetter(window[flag]);
       }
+      console.log("----updateData------inside if");
+
       window.setTimeout(() => { updateData(cfg); }, 1000);
     } else if (Object.hasOwn(window, flag) && window[flag] === false) {
+      console.log("----updateData------inside else");
       flagSetter(window[flag]);
       // query complete, hide loading graphic
       // data = window.dashboard[dataEndpoint].results.data;

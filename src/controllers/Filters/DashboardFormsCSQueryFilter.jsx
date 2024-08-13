f67abc103d45be1afe4954ab7190f9cd13ddceaf
Subodh Jha <subodhj@Subodhs-Laptop.local>
2024-08-13 11:22:22 +0530

    Adding CS RUM Data

--- src/controllers/Filters/DashboardFormsCSQueryFilter.jsx
+++ src/controllers/Filters/DashboardFormsCSQueryFilter.jsx
@@ -254,7 +254,7 @@ export function DashboardFormsCSQueryFilter({
                     <ComboBox
                         name='tenantUrl'
                         label="Tenant"
-                        defaultValue={tenantUrl}
+                        defaultInputValue={tenantUrl}
                         selectedKey={tenantUrl} // Control the selected value
                         onSelectionChange={handleChange} // Update the state on selection change                        
                         autoFocus
